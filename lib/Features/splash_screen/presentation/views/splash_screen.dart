f40986d32b66db9e2bc2c3662eb838dac12d7055
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-09-29 15:05:44 +0300

    splash screen done

--- lib/Features/splash_screen/presentation/views/splash_screen.dart
+++ lib/Features/splash_screen/presentation/views/splash_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:runway_fashion/Core/utils/assets.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1500));
+
+    _fadeAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+
+    _animationController.forward();
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed && mounted) {}
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            spacing: 4,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(Assets.imagesRunway),
+              AnimatedBuilder(
+                animation: _fadeAnimation,
+                builder: (context, child) {
+                  return ShaderMask(
+                    shaderCallback: (bounds) {
+                      return LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [Colors.white.withAlpha(255), Colors.white.withAlpha(0)],
+                        stops: [_fadeAnimation.value, _fadeAnimation.value + 0.05],
+                      ).createShader(bounds);
+                    },
+                    blendMode: BlendMode.dstIn,
+                    child: child,
+                  );
+                },
+                child: SvgPicture.asset(Assets.imagesUnderLine),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
