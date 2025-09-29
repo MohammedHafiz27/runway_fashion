79309ac0f3bc0f6d6f3eb69bfce4a3d08598e21f
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-09-29 14:26:13 +0300

    assets file done

---
+++ lib/Core/utils/assets.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String imagesClassicTailoredFitMensDressShirt =
+      "assets/images/Classic Tailored Fit Men's Dress Shirt.png";
+
+  static const String imagesDeals = "assets/images/deals.png";
+
+  static const String imagesDeepGrayEssential = "assets/images/deep_gray_essential.png";
+
+  static const String imagesGrayCoatAndWhiteTShirtpng = "assets/images/gray_coat_and_white _t-shirtpng.png";
+
+  static const String imagesGreenCoatAndWhiteTShirt = "assets/images/green_coat_and_white_t-shirt.png";
+
+  static const String imagesHome = "assets/images/home.png";
+
+  static const String imagesKids = "assets/images/kids.png";
+
+  static const String imagesLightweightMenPufferJacket = "assets/images/Lightweight Men's Puffer Jacket.png";
+
+  static const String imagesMen = "assets/images/men.png";
+
+  static const String imagesRunway = "assets/images/Runway.svg";
+
+  static const String imagesTopManBlackWithTrous = "assets/images/top_man_black_with_trous.png";
+
+  static const String imagesTopMenBlack = "assets/images/top_men_black.png";
+
+  static const String imagesUnderLine = "assets/images/under_line.svg";
+
+  static const String imagesWhiteTShirt = "assets/images/white_T-shirt.png";
+
+  static const String imagesWoman = "assets/images/woman.png";
+}
