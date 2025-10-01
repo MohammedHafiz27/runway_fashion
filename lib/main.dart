b4453a168829e368fdea73b72370c2c4fff50349
Mohammed Hafiz <Mohammed.Hafiz.2710@gmail.com>
2025-10-01 18:12:05 +0300

    remove unused dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:runway_fashion/Core/cubit/theme_cubit.dart';
@@ -6,7 +5,7 @@ import 'package:runway_fashion/Core/utils/app_route.dart';
 import 'package:runway_fashion/Core/utils/app_theme.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) => const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
